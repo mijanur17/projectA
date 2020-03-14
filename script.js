91739c3c96117837751f9cf55f950b55ba85f11d
mijanur17 <38615142+mijanur17@users.noreply.github.com>
2020-03-14 20:07:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+/* var button = document.getElementById("btn")
+button.addEventListener('onclick', getVal)
+var getVal = (function() {
+    var obj = ( function( thisId ) {
+    var myObj = document.getElementById( thisId ) || document.all[ thisId ];
+      return myObj;
+      } );
+    var newtext2 = obj( "reply" ).value;
+      obj( "layer1" ).innerHTML = (( newtext2 ) ? newtext2 : "null" );
+
+    });
+*/
+    const input = document.querySelector('input');
+    const log = document.getElementById('log');
+
+    input.addEventListener('change', updateValue);
+
+    function updateValue(e) {
+      log.textContent = e.target.value;
+    }
+
+    const selectElement = document.querySelector('.ice-cream');
+
+    selectElement.addEventListener('change', (event) => {
+      const result = document.querySelector('.result');
+      result.textContent = `You like ${event.target.value}`;
+    });
